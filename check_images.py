e2dedac8cd449907c5fddf81b0c1540b19e8145c
Langlish51 <156151012+Langlish51@users.noreply.github.com>
2024-02-23 16:16:14 +1100

    Update check_images.py

--- check_images.py
+++ check_images.py
@@ -3,9 +3,9 @@
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: Franck Tujek
+# DATE CREATED: 28/12/20203                                 
+# REVISED DATE: 22/02/2024
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
 #          summarizes how well the CNN performed on the image classification task. 
